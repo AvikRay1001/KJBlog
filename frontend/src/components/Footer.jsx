6fb7ae039441bf93aa80792bc7af76d005349c3e
AvikRay1001 <avikray1001@gmail.com>
2025-02-10 20:18:02 +0530

    Done

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaYoutube } from "react
 				<div className="">
 					<p className="font-jockey text-[20px]">Student Coordinators:</p>
 					<br/>
-					<div className="flex gap-4 text-black">
+					<div className="flex gap-4 text-white">
 						<div className="">
 							<p className="font-jockey text-[20px]">DHANANJAY MOHANTY</p>
 							<p className="font-jockey">+91 79825 63364</p>
@@ -19,7 +19,7 @@ import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaYoutube } from "react
 						</div>
 					</div>
 				</div>
-				<div className="flex flex-col text-black">
+				<div className="flex flex-col text-white">
 					<p className="font-jockey text-[20px]">Faculty Coordinators:</p><br/>
 					<p className="font-jockey text-[20px]">DR. SAROJ KUMAR DASH</p>
 				</div>
